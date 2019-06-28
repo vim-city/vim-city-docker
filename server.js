4d42f106a7f7593785dd212149053ee86f96b8c6
Taylor Thompson <ttaythompson@gmail.com>
2019-06-28 17:27:44 -0400

    docker setup for vim city

---
+++ server.js
@@ -0,0 +1,33 @@
+"use strict";
+
+const express = require("express");
+const cors = require("cors");
+
+// Constants
+const PORT = 8080;
+const HOST = "0.0.0.0";
+
+// App
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(cors());
+app.options("/", cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello world\n");
+});
+
+app.put("/eval", async (req, res) => {
+  try {
+    const userFunc = await eval(req.body.func);
+
+    res.json(userFunc());
+  } catch (error) {
+    console.log("this is the error in the docker: ", error);
+  }
+});
+
+app.listen(PORT, HOST);
+console.log(`Running on http://${HOST}:${PORT}`);
