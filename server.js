f4e285ef8418c2ea2cc7eac25dcec3f0149c4181
Nikki Bergamini <nikkibergamini@gmail.com>
2019-07-08 15:55:20 -0400

    enabled CORS pre-flight. whitelisted production url

--- server.js
+++ server.js
@@ -13,14 +13,32 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(cors());
-app.options("/", cors());
+// app.use(cors());
+app.options("/eval", cors());
 
-app.get("/", (req, res) => {
+
+// const corsOptions = {
+//   origin: 'https://vim-city.herokuapp.com',
+//   optionsSuccessStatus: 200
+// }
+
+const whitelist = ['https://vim-city.herokuapp.com']
+const corsOptions = {
+ origin: function (origin, callback) {
+   if (whitelist.indexOf(origin) !== -1) {
+     callback(null, true)
+   } else {
+     callback(new Error('Not allowed by CORS'))
+   }
+ }
+}
+
+
+app.get("/", cors(corsOptions),  (req, res) => {
   res.send("Hello world\n");
 });
 
-app.put("/eval", async (req, res) => {
+app.put("/eval", cors(corsOptions), async (req, res) => {
   try {
     console.log('req.body.challengeId', req.body.challengeId)
     console.log('req.body.userInputStr', req.body.userInputStr)
